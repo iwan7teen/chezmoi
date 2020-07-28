27aae68d325d8d9e2edb2603f891efe607537147
Tom Payne <twpayne@gmail.com>
2020-07-28 22:12:22 +0200

    snapshot

--- next/cmd/onepassword.go
+++ next/cmd/onepassword.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/twpayne/chezmoi/next/internal/chezmoi"
 )
@@ -13,22 +14,21 @@ type onepasswordConfig struct {
 	Command string
 }
 
-var (
-	onepasswordCache         = make(map[string]interface{})
-	onepasswordDocumentCache = make(map[string]string)
-)
+var onepasswordOutputCache = make(map[string][]byte)
 
 func init() {
 	config.addTemplateFunc("onepassword", config.onepasswordFunc)
 	config.addTemplateFunc("onepasswordDocument", config.onepasswordDocumentFunc)
+	config.addTemplateFunc("onepasswordDetailsFields", config.onepasswordDetailsFieldsFunc)
 }
 
-func (c *Config) onepasswordFunc(item string) interface{} {
-	if data, ok := onepasswordCache[item]; ok {
-		return data
+func (c *Config) onepasswordOutput(args []string) []byte {
+	key := strings.Join(args, "\x00")
+	if output, ok := onepasswordOutputCache[key]; ok {
+		return output
 	}
+
 	name := c.Onepassword.Command
-	args := []string{"get", "item", item}
 	cmd := exec.Command(name, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
@@ -36,27 +36,66 @@ func (c *Config) onepasswordFunc(item string) interface{} {
 	if err != nil {
 		panic(fmt.Errorf("%s %s: %w\n%s", name, chezmoi.ShellQuoteArgs(args), err, output))
 	}
-	var data interface{}
-	if err := json.Unmarshal(output, &data); err != nil {
-		panic(fmt.Errorf("%s %s: %w\n%s", name, chezmoi.ShellQuoteArgs(args), err, output))
+
+	onepasswordOutputCache[key] = output
+	return output
+}
+
+func (c *Config) onepasswordFunc(args ...string) map[string]interface{} {
+	key, vault := onepasswordGetKeyAndVault(args)
+	onepasswordArgs := []string{"get", "item", key}
+	if vault != "" {
+		onepasswordArgs = append(onepasswordArgs, "--vault", vault)
 	}
-	onepasswordCache[item] = data
+	output := c.onepasswordOutput(onepasswordArgs)
+	var data map[string]interface{}
+	if err := json.Unmarshal(output, &data); err != nil {
+		panic(fmt.Errorf("%s %s: %w\n%s", c.Onepassword.Command, chezmoi.ShellQuoteArgs(onepasswordArgs), err, output))
+	}
 	return data
 }
 
-func (c *Config) onepasswordDocumentFunc(item string) interface{} {
-	if output, ok := onepasswordDocumentCache[item]; ok {
-		return output
+func (c *Config) onepasswordDocumentFunc(args ...string) string {
+	key, vault := onepasswordGetKeyAndVault(args)
+	onepasswordArgs := []string{"get", "document", key}
+	if vault != "" {
+		onepasswordArgs = append(onepasswordArgs, "--vault", vault)
 	}
-	name := c.Onepassword.Command
-	args := []string{"get", "document", item}
-	cmd := exec.Command(name, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	output, err := c.baseSystem.IdempotentCmdOutput(cmd)
-	if err != nil {
-		panic(fmt.Errorf("%s %s: %w\n%s", name, chezmoi.ShellQuoteArgs(args), err, output))
-	}
-	onepasswordDocumentCache[item] = string(output)
+	output := c.onepasswordOutput(onepasswordArgs)
 	return string(output)
+}
+
+func (c *Config) onepasswordDetailsFieldsFunc(args ...string) map[string]interface{} {
+	key, vault := onepasswordGetKeyAndVault(args)
+	onepasswordArgs := []string{"get", "item", key}
+	if vault != "" {
+		onepasswordArgs = append(onepasswordArgs, "--vault", vault)
+	}
+	output := c.onepasswordOutput(onepasswordArgs)
+	var data struct {
+		Details struct {
+			Fields []map[string]interface{} `json:"fields"`
+		} `json:"details"`
+	}
+	if err := json.Unmarshal(output, &data); err != nil {
+		panic(fmt.Errorf("%s %s: %w\n%s", c.Onepassword.Command, chezmoi.ShellQuoteArgs(onepasswordArgs), err, output))
+	}
+	result := make(map[string]interface{})
+	for _, field := range data.Details.Fields {
+		if designation, ok := field["designation"].(string); ok {
+			result[designation] = field
+		}
+	}
+	return result
+}
+
+func onepasswordGetKeyAndVault(args []string) (string, string) {
+	switch len(args) {
+	case 1:
+		return args[0], ""
+	case 2:
+		return args[0], args[1]
+	default:
+		panic(fmt.Sprintf("expected 1 or 2 arguments, got %d", len(args)))
+	}
 }
